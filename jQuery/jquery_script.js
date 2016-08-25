48cbd5855abb9ccf16a2f7225b49819123d44021
ritikasantara <rits.santara@gmail.com>
2016-08-26 00:10:14 +0530

    text_background fix

---
+++ jQuery/jquery_script.js
@@ -0,0 +1,22 @@
+
+
+$(document).ready(function(){
+
+
+
+var pos = $("#text_background");
+
+$(".navbar-collapse").on("click", function() {
+    
+
+    pos.toggleClass("fix");
+
+
+  });
+
+
+
+  
+
+
+});
